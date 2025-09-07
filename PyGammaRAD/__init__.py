219713325d505d2e318c87a637a9d5b3ad49cc90
Aaron Hurst <amhurst@berkeley.edu>
2025-09-07 14:15:15 -0700

    Updated module and added welcome message

--- PyGammaRAD/__init__.py
+++ PyGammaRAD/__init__.py
@@ -3,6 +3,7 @@ from .tables import *
 from .am_formulae import *
 from .am_methods import *
 from .angular_distributions import *
+from .log_handlers import *
 
 __version__='0.1.0'
 __author__='Aaron M. Hurst'
@@ -12,3 +13,5 @@ def get_data(path):
     """Method to return absolute path of the data files inside the root of 
     the Python package."""
     return os.path.join(_ROOT, path)
+
+logger.info(f"\n-------------------------------------------------------\n Welcome to PyGammaRAD v.{__version__} \n\n To write logs to file during session call the method:\n\n PyGammaRAD.write_logs() \n\n For an overview of the project visit the repo: \n\n https://github.com/AaronMHurst/PyGammaRAD \n-------------------------------------------------------")
