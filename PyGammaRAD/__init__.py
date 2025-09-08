2586b148881e820db1215b2e9f61364d30fab3b9
Aaron Hurst <amhurst@berkeley.edu>
2025-09-08 16:07:20 -0700

    Bumped minor version number

--- PyGammaRAD/__init__.py
+++ PyGammaRAD/__init__.py
@@ -5,7 +5,7 @@ from .am_methods import *
 from .angular_distributions import *
 from .log_handlers import *
 
-__version__='0.1.0'
+__version__='0.2.0'
 __author__='Aaron M. Hurst'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
