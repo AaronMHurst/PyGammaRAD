223aad031246ba1efc05e07e2a751a2be5322ece
Aaron Hurst <amhurst@berkeley.edu>
2025-09-08 16:07:09 -0700

    Bumped minor version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = ["numpy", "pandas"]
 
 setuptools.setup(
     name="PyGammaRAD",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/AaronMHurst/PyGammaRAD",
     author="Aaron M. Hurst",
     author_email="amhurst@berkeley.edu",
